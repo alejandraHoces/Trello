166f997c4876695360548725900e50aa726b02f7
alejandraHoces <hocesalejandra@gmail.com>
2017-11-17 11:08:02 -0500

    Añadiendo version 0.0.1

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
-window.addEventListener('load', function(event){
-  var newInput =document.getElementsByClassName('inputtext')[0];
-   function newform() {
-     var createForm = document.createElement('form');
-     var createinput = document.createElement('INPUT')
-     createinput.setAttribute('type', 'text');
-     createinput.setAttribute('value', 'Añadir una lista...');
+function constructor(lista,boton,texto) {
+	this.lista = lista
+	this.boton = boton
+	this.texto = texto
+}
 
-     var createbutton = document.createElement("BUTTON");
-     var text = document.createTextNode("guardar");
-      createbutton.appendChild(text);
-      createForm.innerHTML = createinput + '<br>' + createbutton;
-   }
-})
-newInput.addEventListener('click',newform);
+	var lista = document.getElementById("inputtext");
+
+
+  lista.addEventListener("click",function(){
+  		var cuadroTexto = document.createElement("textarea");
+  		cuadroTexto.setAttribute("class", "el-texto");
+  		var boton = document.createElement("button");
+  		boton.setAttribute("class","el-boton")
+  		var addBoton = document.createTextNode("Agregar");
+  		var cont = document.getElementsByClassName("contenedor")[0];
+  		cont.appendChild(cuadroTexto);
+  		cont.appendChild(boton);
+  		boton.appendChild(addBoton);
+
+      });
