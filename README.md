c812bcb08cdc2496cd73b96f9f9cef848504c700
alejandraHoces <hocesalejandra@gmail.com>
2017-11-19 01:09:21 -0500

    Añadiendo version 0.0.1

---
+++ README.md
@@ -0,0 +1,9 @@
+## **RETO DE CODIGO TRELLO**  
+En el siguiente trabajo se realizó una replica de la pagina web Trello.  
+![Producto final twitter](assets/images/producto.png)
+
+### Herramientas  
+Se utilizó:
+- CSS  
+- HTML  
+- JAVASCRIPT  
--- js/app.js
+++ js/app.js
@@ -1,131 +1,124 @@
-;(function(){
+;(function() {
+  var addForm = document.getElementById('addForm');
+	 var button = document.getElementById('button');
+	 var input = document.getElementById('input');
+	 var firstForm = document.getElementById('firstForm');
+	 var container = document.getElementById('container');
+	 var accountant = 1;
 
-	var addForm = document.getElementById("addForm");
-	var button = document.getElementById("button");
-	var input = document.getElementById("input");
-	var firstForm = document.getElementById("firstForm");
-	var container = document.getElementById("container");
-	var contador = 1;
+	 window.addEventListener('load', loadpage);
 
-	window.addEventListener("load", cargar);
+	 function loadpage() {
+		 addForm.addEventListener('click', function() {
+			 hideElement(firstForm, addForm);
+			 input.focus();
+			 input.value = '';
+		 });
 
-	function cargar(){
-		addForm.addEventListener("click", function(){
-			hideElement(firstForm,addForm);
-			input.focus();
-			input.value="";
-		});
+		 button.addEventListener('click', function(e) {
+			 e.preventDefault();
+			 var listContainer = document.createElement('div');
+			 listContainer.classList.add('d-inlineblock');
 
-		button.addEventListener("click", function(e){
-			e.preventDefault();
-			var contenedorLista = document.createElement("div");
-			contenedorLista.classList.add("d-inlineblock");
+			 var remover = firstForm.parentNode;
+			 container.appendChild(listContainer);
+			 listContainer.appendChild(firstForm);
+			 listContainer.appendChild(addForm);
+			 remover.remove();
 
-			var remover = firstForm.parentNode;
-			container.appendChild(contenedorLista);
-			contenedorLista.appendChild(firstForm);
-			contenedorLista.appendChild(addForm);
-			remover.remove();
+			 var containerCards = document.createElement('div');
+			 containerCards.classList.add('trello-body');
+			 container.insertBefore(containerCards, container.lastElementChild);
+			 containerCards.addEventListener('dragleave', dejarTrello);
+			 containerCards.addEventListener('dragover', arrastrarSobreTrello);
+			 containerCards.addEventListener('drop', soltarTrello);
+			 containerCards.addEventListener('dragend', terminaArrastrarTrello);
 
-			var contenedorTarjetas = document.createElement("div");
-			contenedorTarjetas.classList.add("trello-body");
-			container.insertBefore(contenedorTarjetas,container.lastElementChild);
-	        contenedorTarjetas.addEventListener("dragleave", dejarTrello);
-	        contenedorTarjetas.addEventListener("dragover", arrastrarSobreTrello);
-			contenedorTarjetas.addEventListener("drop", soltarTrello);
-			contenedorTarjetas.addEventListener("dragend", terminaArrastrarTrello);
+			 hideElement(firstForm, addForm);
 
-			hideElement(firstForm,addForm);
+			 newElements('div', 'newlist', input.value, containerCards);
+			 newElements('div', 'add', 'Añadir una tarjeta', containerCards);
 
-			crearElementos("div", "newlist", input.value, contenedorTarjetas);
-			crearElementos("div", "add", "Añadir una tarjeta", contenedorTarjetas);
+			 var add = document.getElementsByClassName('add');
+			 add[add.length - 1].addEventListener('click', function() {
+				 this.classList.add('d-none');
+				 newForm('form', 'fomulario', containerCards, this);
+			 });
+		 });
+	 }
 
-			var add = document.getElementsByClassName("add");
-			add[add.length-1].addEventListener("click", function(){
-				this.classList.add("d-none");
-				newForm("form", "fomulario", contenedorTarjetas,this);
-			});
+	 function hideElement(a, b) {
+		 a.classList.toggle('d-none');
+		 b.classList.toggle('d-none');
+	 }
 
-		});
-	}
+	 function newElements(element, clase, texto, container) {
+		 var div = document.createElement(element);
+		 div.classList.add(clase);
+		 div.innerHTML = texto;
+		 container.appendChild(div);
+	 }
 
-	function hideElement(a,b){
-			a.classList.toggle("d-none");
-			b.classList.toggle("d-none");
-	}
+	 function newForm(form, clase, container, agregarTarjeta) {
+		 var form = document.createElement(form);
+		 form.classList.add(clase);
+		 newElements('textarea', 'textarea', '', form);
+		 newElements('button', 'boton', 'Añadir', form);
+		 container.appendChild(form);
 
-	function crearElementos(element, clase, texto, container){
-		var div = document.createElement(element);
-		div.classList.add(clase);
-		div.innerHTML= texto;
-		container.appendChild(div);
-	}
+		 form.lastElementChild.addEventListener('click', function(e) {
+			 e.preventDefault();
+			 agregarTarjeta.classList.remove('d-none');
+			 form.classList.add('d-none');
 
-	function newForm(form, clase, container, agregarTarjeta){
-		var form = document.createElement(form);
-		form.classList.add(clase);
-		crearElementos("textarea","textarea","", form);
-		crearElementos("button", "boton", "Añadir", form);
-		container.appendChild(form);
+			 var text = form.firstElementChild.value;
 
-		form.lastElementChild.addEventListener("click", function(e){
-			e.preventDefault();
-			agregarTarjeta.classList.remove("d-none");
-			form.classList.add("d-none");
-
-			var text = form.firstElementChild.value;
-
-			var div = document.createElement("div");
-			div.classList.add("text-cards");
-			div.draggable = true;
-	        div.setAttribute("id", "id" + contador);
-			div.innerHTML = text;
-			contador ++;
-			div.addEventListener("dragstart", empiezaArrastrar);
-			div.addEventListener("drop", soltar);
-			div.addEventListener("dragend", terminaArrastrar);
-			container.insertBefore(div, agregarTarjeta);
-
-		});
-	}
+			 var div = document.createElement('div');
+			 div.classList.add('text-cards');
+			 div.draggable = true;
+			 div.setAttribute('id', 'id' + accountant);
+			 div.innerHTML = text;
+			 accountant ++;
+			 div.addEventListener('dragstart', empiezaArrastrar);
+			 div.addEventListener('drop', soltar);
+			 div.addEventListener('dragend', terminaArrastrar);
+			 container.insertBefore(div, agregarTarjeta);
+		 });
+	 }
 
 
-	function empiezaArrastrar(e) {
-		e.dataTransfer.setData("text", this.id);
-		this.classList.add("opacidad");
+	 function empiezaArrastrar(e) {
+		 e.dataTransfer.setData('text', this.id);
+		 this.classList.add('opacidad');
+	 }
 
-	}
+	 function arrastrarSobreTrello(e) {
+		 e.preventDefault();
+		 this.classList.add('bg');
+	 }
 
-	function arrastrarSobreTrello(e) {
-		e.preventDefault();
-		this.classList.add("bg");
-	}
-
-	function dejarTrello(e) {
-		e.preventDefault();
-		this.classList.remove("bg");
-	}
+	 function dejarTrello(e) {
+		 e.preventDefault();
+		 this.classList.remove('bg');
+	 }
 
 
-	function soltar(e) {
+	 function soltar(e) {
 	   e.preventDefault();
+	 }
 
-	}
-
-	function soltarTrello(e) {
+	 function soltarTrello(e) {
 	   e.preventDefault();
-	   var arrastrado = e.dataTransfer.getData("text");
+	   var arrastrado = e.dataTransfer.getData('text');
 	   var elemento = document.getElementById(arrastrado);
 	   this.insertBefore(elemento, this.children[1]);
-	}
+	 }
+	 function terminaArrastrarTrello(e) {
+		 this.classList.remove('bg');
+	 }
 
-	function terminaArrastrarTrello(e){
-		this.classList.remove("bg");
-	}
-
-	function terminaArrastrar(e) {
-		this.classList.remove("opacidad");
-		this.classList.add("animated", "bounceIn");
-	}
-
+	 function terminaArrastrar(e) {
+		 this.classList.remove('opacidad');
+		 this.classList.add('animated', 'bounceIn');
+	 }
 }());
